b3a2e99c9568f584642e1df383c37fe6aaed214d
srijanmalhotra2711 <srijan.malhotra2711@gmail.com>
2021-07-16 19:18:33 +0530

    16th July 2021

---
+++ location-extractor/Connect.py
@@ -0,0 +1,10 @@
+import mysql.connector
+from nltk.tokenize import RegexpTokenizer
+
+tokenizer = RegexpTokenizer("[\w']+")
+mydb = mysql.connector.connect(host='10.0.0.151',user='etl_readonly_V3',password='rdelTonlyEvVer3',db='ev_dw')
+mycursor = mydb.cursor()
+mycursor.execute("SELECT * FROM flat_ed_places WHERE key_id=50 ")
+result = mycursor.fetchall()
+for i in result:
+    tokenizer.tokenize(print(i))
