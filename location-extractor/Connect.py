640c6c9a5fb653724b5ddb3b0d0820396121ae6b
srijanmalhotra2711 <srijan.malhotra2711@gmail.com>
2021-07-16 19:23:12 +0530

    16th July 2021

--- location-extractor/Connect.py
+++ location-extractor/Connect.py
@@ -1,6 +1,6 @@
 import mysql.connector
 from nltk.tokenize import RegexpTokenizer
-
+#Tokeniser
 tokenizer = RegexpTokenizer("[\w']+")
 mydb = mysql.connector.connect(host='10.0.0.151',user='etl_readonly_V3',password='rdelTonlyEvVer3',db='ev_dw')
 mycursor = mydb.cursor()
