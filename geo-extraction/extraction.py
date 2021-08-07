5675dc8b02cbbb53c4468cd23c5779dbd1c706f8
srijanmalhotra2711 <srijan.malhotra2711@gmail.com>
2021-08-08 00:06:55 +0530

    8th August 2021

--- geo-extraction/extraction.py
+++ geo-extraction/extraction.py
@@ -3,7 +3,7 @@ import re
 import os
 import io
 
-_ROOT = os.path.abspath(os.path.dirname(__file__))
+_ROOT = os.path.abspath(os.path.dirname(__file__)) 
 
 def get_data_path(path):
     return os.path.join(_ROOT, 'data', path)
